c10e3b42dfeb58f959bde7feb996887a5189764f
SammyAgrawal <41808786+SammyAgrawal@users.noreply.github.com>
2022-10-23 14:54:29 -0400

    Update polynomial
    make better

--- polynomial.py
+++ polynomial.py
@@ -1,7 +1,11 @@
 degree = (int(input("whats the degree ")))+1
 total = 0
-x= int(input("What are u plugging into function, bakayaro, konayaro "))
+poly = []
 for i in range(degree):
     coeff= int(input("Enter coefficient for degree "+ str(i) + " "))
-    total += coeff*(x**i)
+    poly.append(coeff)
+x= int(input("What are u plugging into function, bakayaro, konayaro "))       
+for i in range(degree):
+    total += poly[i] * x**i
+    
 print(total)
